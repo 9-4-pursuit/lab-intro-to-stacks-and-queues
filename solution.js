708c675d8d74ea5dbe96079359d0d856a8350e4d
Mark Robertson <markrobertson67@gmail.com>
2023-07-02 14:40:02 -0400

    First commit

--- solution.js
+++ solution.js
@@ -1,8 +1,52 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
-module.exports = {
-  Node,
-  Queue,
-  Stack,
-};
+
+
+  class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  class Stack {
+    constructor(top = null) {
+      this.top = top;
+    }
+
+
+    
+    push(data) {
+      // O(1)
+      const newItem = new Node(data); // Creta a new node
+      newItem.next = this.top; // set 'next' on our new node to be the top of the stack
+      this.top = newItem; // set the top of the stack to be our new node
+    }
+
+
+  }
+
+
+
+
+
+
+
+  class Queue {
+    constructor() {
+      this.first = null;
+      this.last = null;
+      this.size = 0;
+    }
+  }
+
+
+
+
+
+
+  module.exports = {
+    Node,
+    Stack
+  };
