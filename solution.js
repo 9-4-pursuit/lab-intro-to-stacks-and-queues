2c50504a2f1a0b7a83e6451ecba77a979117b215
ninajavier <ninajavier13@gmail.com>
2023-07-02 11:51:41 -0400

    Added node and stack classes, added push, pod, size, isEmpty, peek and findMin methods

--- solution.js
+++ solution.js
@@ -1,8 +1,71 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(top = null) {
+    this.top = top;
+    this.min = [];
+  }
+
+  push(data) {
+    if (this.min.length === 0 || data <= this.min[this.min.length -1]) {
+      this.min.push(data);
+    }
+    const newItem = new Node(data); 
+    newItem.next = this.top; 
+    this.top = newItem; 
+  }
+
+  pop() {
+    if (this.top === null) {
+      throw new Error("The Stack is Empty");
+    }
+    let item = this.top;
+    let newItem = item.next;
+    this.top = newItem;
+    return item;
+  }
+
+  size() {
+    let count = 0;
+    let currentNode = this.top;
+    while (currentNode != null) {
+      count++;
+      currentNode = currentNode.next;
+    }
+    return count;
+  }
+
+  isEmpty() {
+    return this.top === null;
+  }
+
+  peek() {
+    if (this.top === null) {
+      throw new Error("The Stack is Empty");
+    }
+    return this.top;
+  }
+
+  findMin() {
+    return this.min.length ? this.min[this.min.length - 1] : 0;
+  }
+
+
+}
+
+
+
+
 module.exports = {
   Node,
-  Queue,
+  // Queue,
   Stack,
 };
