d275232a7d38417e4d28588337b6b6766246b348
SuperNinjaEv <evan0210@gmail.com>
2023-07-02 11:43:33 -0400

    Setup classes and completed push method for stack

--- solution.js
+++ solution.js
@@ -1,6 +1,43 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  };
+};
+
+class Stack {
+  constructor(top = null) {
+    this.top = top;
+  };
+
+  push(data) {
+    const newItem = new Node(data);
+    newItem.next = this.top;
+    this.top = newItem;
+  };
+
+  
+  
+};
+
+
+
+
+class Queue {
+  constructor(first = null) {
+    this.first = first;
+    this.last = null;
+    this.size = 0;
+    this.max = 0;
+  };
+};
+
+
+
+
 module.exports = {
   Node,
   Queue,
