ff5d26cc3cf100b49d9bb709a66b7d6cc3d2108d
BertCasale <bthebest55@gmail.com>
2023-07-02 11:06:42 -0400

    base for classes

--- solution.js
+++ solution.js
@@ -1,6 +1,24 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+    constructor() {
+
+    }
+}
+
+class Stack {
+    constructor() {
+        
+    }
+}
+
+class Queue {
+    constructor() {
+        
+    }
+}
+
 module.exports = {
   Node,
   Queue,
