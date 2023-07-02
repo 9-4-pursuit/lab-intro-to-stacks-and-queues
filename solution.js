0e399194a69b76e0dee42e1ea3bbc38d0e56c31d
Mark Robertson <markrobertson67@gmail.com>
2023-07-02 15:25:16 -0400

    5 passing

--- solution.js
+++ solution.js
@@ -2,7 +2,6 @@ const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
 
-
   class Node {
     constructor(data) {
       this.data = data;
@@ -15,7 +14,6 @@ const { inspect } = require("util");
       this.top = top;
     }
 
-
     
     push(data) {
       // O(1)
@@ -24,12 +22,35 @@ const { inspect } = require("util");
       this.top = newItem; // set the top of the stack to be our new node
     }
 
+    size() {
+      let count = 0;
+      let current = this.top;
+      while (current !== null) {
+        count++;
+        current = current.next;
+      }
+      return count;
+    }
+    
+    
+
+
+
+
+    pop() {
+      if (this.top === null) {
+        throw new Error("The Stack is Empty");
+      }
+  
+      let item = this.top; // item = december
+      let newItem = item.next; // newItem = november
+      this.top = newItem; // this.top = november
+      // this.top = this.top.next;
+      return item;
+    }
+    
 
   }
-
-
-
-
 
 
 
@@ -40,10 +61,6 @@ const { inspect } = require("util");
       this.size = 0;
     }
   }
-
-
-
-
 
 
   module.exports = {
