e80894ca5cbfd2d7684e68b33e09ad0eebbe6b47
pjungjs <pjungjs@gmail.com>
2023-07-04 00:11:05 -0400

    add Queue methods

--- solution.js
+++ solution.js
@@ -78,6 +78,57 @@ class Queue {
     this.size = 0;
     this.max = null;
   }
+
+  enqueue(data) {
+    let newItem = new Node(data);
+    if (this.first === null) {
+      this.first = newItem;
+      this.last = newItem;
+    } else {
+      this.last.next = newItem;
+      this.last = newItem;
+    }
+    return this.size++;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) throw new Error("The Queue is Empty");
+    const removedItem = this.first;
+    if (this.first === this.last) this.last = null;
+    this.first = this.first.next;
+    this.size--;
+    return removedItem.data;
+  }
+
+  count() {
+    return this.size;
+  }
+
+  isEmpty() {
+    return this.first === null;
+  }
+
+  peek() {
+    if (this.isEmpty()) throw new Error("The Queue is Empty");
+    return this.first;
+  }
+
+  getLast() {
+    if (this.isEmpty()) throw new Error("The Queue is Empty");
+    return this.last;
+  }
+
+  findMax() {
+    if (this.isEmpty()) throw new Error("The Queue is Empty");
+    let currentNode = this.first;
+    while (currentNode) {
+      if (this.max === null || currentNode.data > this.max) {
+        this.max = currentNode.data;
+      }
+      currentNode = currentNode.next;
+    }
+    return this.max;
+  }
 }
 
 
